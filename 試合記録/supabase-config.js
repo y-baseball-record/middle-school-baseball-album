1215f9844da2c146cc252f23b60be1bc2e82348b
y-baseball-record <hoshi.sola82@gmail.com>
2026-09-23 16:33:48 +0900

    最新の野球記録アプリをアップロード

---
+++ 試合記録/supabase-config.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://xnqoxstvchavfimfmatf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_rwk1QPMi7WIHj-i7RJNmOQ_OkVadcf3";
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
